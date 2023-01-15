f42cb8aa1922e452763595253545581ab625bd1f
Matheus Castro <matheusbjorn@gmail.com>
2023-01-15 06:12:32 -0300

    feat: started project

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const cors = require('cors');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors())
+var TrackerSchema = mongoose.Schema({
+    Title: String,
+    Year: String,
+    Type: String,
+    Poster: String,
+    imdbID: String,
+    Status: 'Watched' | 'To Watch' | 'In Progress'
+});
+
+var Tracker = mongoose.model('Tracker', TrackerSchema, 'trackerstore');
+
+app.get("/", (req, res) => {
+    res.json({ message: 'Hello World!' })
+});
+
+const newsRoutes = require('./routes/index.js');
+
+app.use('/movies', newsRoutes);
+
+app.post('/tracker', (req, res) => {
+    try {
+        var tracker1 = new Tracker(req.body);
+
+        tracker1.save(function (err, tracker) {
+            if (err) return console.error(err);
+            console.log(tracker.Title + " saved to trackerstore collection.");
+            res.status(200).send()
+        });
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e);
+    }
+})
+
+app.get('/tracker', async (req, res) => {
+    try {
+        Tracker.find({}, function(err, trackers) {
+          var trackerMap = [];
+      
+          trackers.forEach(function(tracker) {
+            trackerMap.push(tracker);
+          });
+      
+          res.send(trackerMap);  
+        });
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e);
+    }
+})
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+const PORT = 3000
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@filminho.qsjn6y6.mongodb.net?retryWrites=true&w=majority`).then(e => {
+    console.log("Conectando ao Banco de dados");
+    app.listen(process.env.PORT || PORT)
+    console.log("Conectado com sucesso, API iniciada na porta", process.env.PORT || PORT);
+})
+    .catch(err => console.error('Não foi possível conectar ao Banco de dados. Erro:', err));
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+const router = require('express').Router();
+
+router.get('', (req, res) => {
+    fetch("https://www.omdbapi.com/?apikey=5f8e75f6&s=" + req.query.title, {
+        "headers": {
+            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cache-control": "max-age=0",
+            "sec-ch-ua": "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"",
+            "sec-ch-ua-mobile": "?1",
+            "sec-ch-ua-platform": "\"Android\"",
+            "sec-fetch-dest": "document",
+            "sec-fetch-mode": "navigate",
+            "sec-fetch-site": "none",
+            "sec-fetch-user": "?1",
+            "upgrade-insecure-requests": "1"
+        },
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET"
+    }).then(e => e.text())
+        .then(e => res.send(e));
+});
+
+router.get('/watched', async (req, res) => {
+    const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }]);
+    console.log('Inserted documents =>', insertResult);
+});
+
+module.exports = router;
